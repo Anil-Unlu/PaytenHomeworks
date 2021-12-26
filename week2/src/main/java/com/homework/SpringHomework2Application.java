922866dfbb674becd77fa8206e343d9468ec0153
Anil-unlu <anl_nl@hotmail.com>
2021-12-26 18:30:58 +0300

    week2

---
+++ week2/src/main/java/com/homework/SpringHomework2Application.java
@@ -0,0 +1,13 @@
+package com.homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHomework2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHomework2Application.class, args);
+	}
+
+}
---
+++ week2/src/main/java/com/homework/deneme.java
@@ -0,0 +1,10 @@
+package com.homework;
+
+public class deneme {
+	public static void mai(String args[]) {
+		System.out.println("asdıjaposofmapsfomağsasofmapğsofmağf");
+	}
+	
+	
+	
+}
